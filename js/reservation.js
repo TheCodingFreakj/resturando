d6058db3f045f8f0764c4073fb4a254474bc81ca
TheCodingFreakj <pallavidapriya75@gmail.com>
2022-11-19 04:51:04 +0530

    done

--- js/reservation.js
+++ js/reservation.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 (function ($) {
   "use strict";
   $("#datepicker")
@@ -13,8 +14,81 @@
     scrollbar: true,
   });
 
-  $("#submit-btn").click(function () {
-    e.preventDefault();
-    $("#my-form").submit(); // Submit the form
-  });
+  // $("#my-form").on("submit", function (e) {
+  //   e.preventDefault(); //prevent default form submition
+  //   var email = $("#my-form").find('input[name="email"]').val();
+  //   var name = $("#my-form").find('input[name="name"]').val();
+  //   var phone = $("#my-form").find('input[name="phone"]').val();
+  //   var guests = $("#my-form").find('input[name="guests"]').val();
+  //   var time = $("#my-form").find('input[name="time"]').val();
+  //   var date = $("#my-form").find('input[name="date"]').val();
+  //   var FormData = {
+  //     email,
+  //     name,
+  //     phone,
+  //     guests,
+  //     time,
+  //     date,
+  //   };
+
+  //   const headers = {
+  //     'Content-Type': 'multipart/form-data',
+  //     "Access-Control-Allow-Origin": "*",
+  //   }
+    
+  //   axios.post("C:/Users/Lenovo/Documents/ThemeForest/Resturando/reservation.php", JSON.stringify(FormData), {
+  //       headers: headers
+  //     })
+  //     .then((response) => {
+  //     console.log(response);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     })
+  //   // var FormData = $("#my-form").serialize();
+  //   //   console.log(FormData);
+  //   //   var settings = {
+  //   //     cache: false,
+  //   //     dataType: "jsonp",
+  //   //     async: true,
+  //   //     crossDomain: true,
+  //   //     url: "../php/reservation",
+  //   //     method: "POST",
+  //   //     data: FormData,
+  //   //     headers: {
+  //   //       accept: "application/json",
+  //   //       "Access-Control-Allow-Origin": "*",
+  //   //     },
+  //   //   };
+  //   //   $.ajax(settings).done(function (response) {
+  //   //     console.log(response);
+  //   //   });
+  //   //   // $.ajax({
+  //   //   //   type: "POST",
+  //   //   //   url: "../php/reservation.php",
+  //   //   //   data: FormData,
+  //   //   //   crossDomain: true,
+
+  //   //   //    dataType: "json",
+  //   //   //   encode: true,
+  //   //   //   beforeSend: function () {
+  //   //   //     $("#submit-button").html(
+  //   //   //       '<span class="glyphicon glyphicon-repeat fast-right-spinner"></span> Sending'
+  //   //   //     );
+  //   //   //   },
+  //   //   //   success: function (response) {
+  //   //   //     console.log(response);
+  //   //   //     // response = JSON.parse(response);
+
+  //   //   //     // if (response == "ok") {
+  //   //   //     //   $("sendmessage").html("Your message has been sent successfully.");
+  //   //   //     // } else {
+  //   //   //     //   $("errormessage").html(response);
+  //   //   //     // }
+  //   //   //   },
+  //   //   // });
+  // });
 })(jQuery);
+
+
+
--- php/config.php
+++ php/config.php
@@ -5,8 +5,11 @@ $password = "RSDdHTjqJf";
 $dbName = "sql12578844";
 // Create database connection
 $conn = new mysqli($host, $userName, $password, $dbName);
+
 // Check connection
 if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
 ?>
+
